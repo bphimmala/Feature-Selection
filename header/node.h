91e08f880569e88f975737c468832c9635c49c54
emilyhix <hix.emily.m@gmail.com>
2024-11-13 15:53:49 -0800

    Update node.h

--- header/node.h
+++ header/node.h
@@ -1,21 +1,27 @@
 #ifndef NODE_H
 #define NODE_H
 #include <cmath>
+#include <vector>
 #include <iostream>
 using namespace std;
-
-enum Operations {MODIFY1, MODIFY2, MODIFY3, MODIFY4};
 
 class Node{
     private:
         double accuracy;    // basically h(n)
-        int initialArray[4];
-        Operations operation;
+        int featureMax;
+        int algorithm;
+        int newFeat;
+        vector<int> currState;
+        vector<int> initialArray;
     public:
-        Node(int);
-        Node(int, Operations, Node);
-        void updateState(int);
+        Node(int, int);  // child constructor
+        Node(int, Node); // parent constructor
+        void updateState();
         double evaluationFunction();
+        bool isValid();
+        bool operator<(const Node &) const; //overloading operators for prio queue
+        bool operator>(const Node &) const;
+        int getFeatureMax();
 };
 
 #endif
