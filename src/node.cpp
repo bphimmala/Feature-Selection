cb8f6665f0adc4d3b36d337747b6d52f9b06aa15
Baipon Phimmala <129914502+bphimmala@users.noreply.github.com>
2024-11-13 16:36:41 -0800

    Updated constructors, evaluation functions, valid function; overloaded comparison operators

--- src/node.cpp
+++ src/node.cpp
@@ -2,7 +2,6 @@
 
 Node::Node(int algo, int featMax) {
     // parent constructor
-    accuracy = evaluationFunction();
     featureMax = featMax;
     algorithm = algo;
     newFeat = 0;
@@ -12,17 +11,37 @@ Node::Node(int algo, int featMax) {
             currState.push_back(i+1);
         }
     }
+    
+    if(algo == 0){
+        accuracy = FSevaluationFunction(currState);
+    }
+    else if(algo == 1){
+        accuracy == BEevaluationFunction(currState);
+    }
 }
 
 Node::Node(int newFeature, Node parent) {
     // child constructor
-    accuracy = evaluationFunction();
 
     featureMax = parent.getFeatureMax();
     algorithm = parent.getAlgorithm();
     newFeat = newFeature;
+
     for (int i = 0; i < parent.currState.size()-1; ++i) {
         currState.push_back(parent.currState.at(i));
+    }
+
+    if (algorithm) {
+        for (int i = 0; i < featureMax; ++i) {
+            currState.push_back(i+1);
+        }
+    }
+    
+    if(algorithm == 0){
+        accuracy = FSevaluationFunction(currState);
+    }
+    else if(algorithm == 1){
+        accuracy == BEevaluationFunction(currState);
     }
 }
 
@@ -31,11 +50,30 @@ void Node::updateState(){
     currState.push_back(newFeat);
 }
 
-double Node::evaluationFunction(vector<int>){
+double Node::BEevaluationFunction(vector<int>){
     const long max_rand = 1000000L;
     double lowerBound = 0;
     double upperBound = 10;
     double randNum = lowerBound + (upperBound - lowerBound) * (rand() % max_rand) / max_rand;
     
     return randNum;
+}
+
+
+bool Node::isValid(){
+    for (int i = 0; i < featureMax; i++){
+        if(i == newFeat){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool Node::operator<(const Node & N) const{
+    return ((accuracy > N.accuracy));
+}
+
+bool Node::operator>(const Node & N) const{
+    return ((accuracy < N.accuracy));
 }
