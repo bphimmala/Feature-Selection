df6d497af38a08c8383fb9e62635086e4e84518f
emilyhix <hix.emily.m@gmail.com>
2024-12-05 19:53:29 -0800

    Add dataset choice to printMenu

--- src/printMenu.cpp
+++ src/printMenu.cpp
@@ -3,6 +3,8 @@
 void printMenu() {
     int userFeatureChoice = 0;
     int userAlgorithmChoice = 0;
+    int userDatasetChoice = 0;
+    string dataset;
     int algorithm = 0;
 
     cout << "Welcome to ehix001-sphim001 Feature Selection Algorithm" << endl << endl;
@@ -25,7 +27,21 @@ void printMenu() {
         algorithm = 1;
     }
     
-    Node initialNode(algorithm, userFeatureChoice);
+    cout << "Type the number of the dataset to use." << endl;
+    cout << "1) Small" << endl;
+    cout << "2) Large" << endl << endl;
+    // cout << "3) Special Algorithm" << endl << endl;
+    cin >> userDatasetChoice;
+    cout << endl;
+
+    if (userDatasetChoice == 1) {
+        dataset = "../include/small-test-dataset.txt";
+    }
+    else if (userDatasetChoice == 2) {
+        dataset = "../include/large-test-dataset.txt";
+    }
+
+    Node initialNode(algorithm, userFeatureChoice, dataset);
     Node solution = Search(initialNode);
     // cout << endl << "(Warning, Accuracy has decreased!)" << endl;
     // cout << "Finished search!! The best feature subset is ";
