ad3b429e4aa50ba6c2c73f5eedfc54a8cf21cf54
Baipon Phimmala <129914502+bphimmala@users.noreply.github.com>
2024-12-05 19:50:24 -0800

    Set up validator

--- header/validator.h
+++ header/validator.h
@@ -1,4 +1,8 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
+#include <vector>
+#include "..\header\node.h"
+
+double validator(vector<int>, const string);
 
 #endif
--- src/validator.cpp
+++ src/validator.cpp
@@ -1,2 +1,35 @@
 #include "../header/validator.h"
 
+double validator(vector<int> featureSubset, const string dataFile){
+
+    // vector<vector<double>> allInstances;
+    // vector<int> featureSubset;
+
+    // int nearestNeighbor;
+    // int correct;
+
+    // for(int i = 0; i < allInstances.size(); i++){ // THIS ONE IS THE ONE WE ARE TESTING
+    //     vector<double> testingInstance = allInstances.at(i);
+    //     double distance = max();
+    //     for(int j = 0; i < allInstances.size(); j++){ // THIS IS WHAT WE'RE TESTING AGAINST
+    //         if(j != i){
+    //             vector<double> comparingInstance = allInstances.at(j);
+    //             double sumSq = 0;
+    //             for(int k = 0; k < featureSubset.size(); k++){ // FINDS THE FEATURES
+    //                 sumSq = sumSq + (testingInstance.at(featureSubset.at(k)) - comparingInstance.at(featureSubset.at(k)))^2;
+    //             }
+    //             double euclideanDist = sqrt(sumSq);
+    //             if(euclideanDist < distance){
+    //                 distance = euclideanDist;
+    //                 nearestNeighbor = j;
+    //             }
+    //         }
+    //     }
+    //     if (allInstances.at(j).at(0) == testingInstance.at(0)) {
+    //         ++correct;
+    //     }
+    // }
+
+    // return correct / allInstances.size();
+    return 0;
+}
