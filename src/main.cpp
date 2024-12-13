e802561a96e272a2545d3a1263d894ffb2add82b
emilyhix <hix.emily.m@gmail.com>
2024-12-12 19:12:27 -0800

    Add best feature subset comment to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+// Group: Emily Hix -- ehix001 -- Lecture Section 001, Sabaipon Phimmala -- sphim001 -- Lecture Section 001
+// DatasetID: Titanic
+// Small Dataset Results:
+// - Forward: Feature Subset: {5, 3}, Acc: 92%
+// - Backward: Feature Subset: {2, 4, 5, 7, 10}, Acc: 82%
+// Large Dataset Results:
+// - Forward: Feature Subset: {27, 1}, Acc: 95.5%
+// - Backward: Feature Subset: {2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
+//                              20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 
+//                              35, 36, 37, 38, 39, 40}, Acc: 72.2%
+
 #include "..\header\printMenu.h"
 
 int main() {
