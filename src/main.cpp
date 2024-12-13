9197a43f2e492074a1d13764c4d58efe64e8a446
bphimmala <bphimmala@gmail.com>
2024-12-12 23:03:16 -0800

    Adding Titanic results to main

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 // - Backward: Feature Subset: {2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 //                              20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 
 //                              35, 36, 37, 38, 39, 40}, Acc: 72.2%
+// Titanic Dataset Results:
+// - Forward: Feature Subset: {2}, Acc: 78.0112%
+// - Backward: Feature Subset: {2}, Acc: 78.0112%
 
 #include "..\header\printMenu.h"
 
